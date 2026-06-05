7ebf140ad7945b27355d2caf92028800069d3181
Unjuno <unjuno.org@unjuno.org>
2026-06-06 03:11:01 +0900

    Add Bonsai text constants

---
+++ scripts/bonsai_text_consts.py
@@ -0,0 +1,9 @@
+PROMPTS = [
+    'a tiny bonsai tree in a ceramic pot',
+    'red bonsai pot on a plain background',
+]
+REPO = 'prism-ml/bonsai-image-binary-4B-unpacked'
+TOKENIZER_SUBFOLDER = 'tokenizer'
+TEXT_ENCODER_SUBFOLDER = 'text_encoder'
+TEXT_HIDDEN_DIM = 2560
+BONSAI_CONTEXT_DIM = 7680
