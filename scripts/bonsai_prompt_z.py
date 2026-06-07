fa787b783d354915916f95ee2f58eab6e7ba2aeb
Unjuno <unjuno.org@unjuno.org>
2026-06-08 00:40:51 +0900

    Use staged loop for prompt latent

--- scripts/bonsai_prompt_z.py
+++ scripts/bonsai_prompt_z.py
@@ -1,8 +1,10 @@
 from bonsai_next import apply_delta
 from bonsai_pred_one import pred_once
-from probe_bonsai_staged_generation_smoke import STEP_SIZE
+from probe_bonsai_staged_generation_smoke import STEP_COUNT,STEP_SIZE
 
 
 def prompt_z():
     z,c,p=pred_once()
-    return apply_delta(z,p,STEP_SIZE),c,p
+    for _ in range(STEP_COUNT):
+        z=apply_delta(z,p,STEP_SIZE)
+    return z,c,p
