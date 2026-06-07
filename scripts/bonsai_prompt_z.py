1bd5a8a1291a072ca7bb7116e3e3669e09b5a8d2
Unjuno <unjuno.org@unjuno.org>
2026-06-08 01:18:01 +0900

    Use per-step prompt prediction loop

--- scripts/bonsai_prompt_z.py
+++ scripts/bonsai_prompt_z.py
@@ -1,10 +1,5 @@
-from bonsai_next import apply_delta
-from bonsai_pred_one import pred_once
-from probe_bonsai_staged_generation_smoke import STEP_COUNT,STEP_SIZE
+from bonsai_prompt_loop import prompt_loop
 
 
 def prompt_z():
-    z,c,p=pred_once()
-    for _ in range(STEP_COUNT):
-        z=apply_delta(z,p,STEP_SIZE)
-    return z,c,p
+    return prompt_loop()
