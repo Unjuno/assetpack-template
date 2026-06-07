539dc52e6faed741d120bca9f2f4860024d0431d
Unjuno <unjuno.org@unjuno.org>
2026-06-08 00:45:33 +0900

    Recompute prompt prediction each staged step

--- scripts/bonsai_prompt_z.py
+++ scripts/bonsai_prompt_z.py
@@ -1,10 +1,13 @@
+from bonsai_ctx_cast import ctx_like
+from bonsai_lowbit_step import load_sd,step
 from bonsai_next import apply_delta
-from bonsai_pred_one import pred_once
+from bonsai_prompt_seed import seeded_inputs
 from probe_bonsai_staged_generation_smoke import STEP_COUNT,STEP_SIZE
 
 
 def prompt_z():
-    z,c,p=pred_once()
+    sd=load_sd(); z,t=seeded_inputs(); c=ctx_like(z)
+    p=None
     for _ in range(STEP_COUNT):
-        z=apply_delta(z,p,STEP_SIZE)
-    return z,c,p
+        p=step(sd,z,c,t)
+        z=apply_delta(z,p,STEP
