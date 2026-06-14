e7677009c9dc3984bccda79a15236e6a72bf3f8c
Unjuno <unjuno.org@unjuno.org>
2026-06-14 21:40:31 +0900

    Delete old script

--- scripts/bonsai_text_model.py
+++
@@ -1,4 +0,0 @@
-def load_text_model():
-    from transformers import AutoModel
-    from bonsai_text_consts import REPO, TEXT_ENCODER_SUBFOLDER
-    return AutoModel.from_pretrained(REPO, subfolder=TEXT_ENCODER_SUBFOLDER).eval()
