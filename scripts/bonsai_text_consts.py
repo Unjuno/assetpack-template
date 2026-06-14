40bd2db3c419c5acf4ae7af9d373817190f3b792
Unjuno <unjuno.org@unjuno.org>
2026-06-14 22:41:25 +0900

    Delete old script

--- scripts/bonsai_text_consts.py
+++
@@ -1,9 +0,0 @@
-PROMPTS = [
-    'a tiny bonsai tree in a ceramic pot',
-    'red bonsai pot on a plain background',
-]
-REPO = 'prism-ml/bonsai-image-binary-4B-unpacked'
-TOKENIZER_SUBFOLDER = 'tokenizer'
-TEXT_ENCODER_SUBFOLDER = 'text_encoder'
-TEXT_HIDDEN_DIM = 2560
-BONSAI_CONTEXT_DIM = 7680
