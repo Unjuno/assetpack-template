00f5405a298a580f7e28ae5659adc135dbaa398f
Unjuno <unjuno.org@unjuno.org>
2026-06-14 22:39:56 +0900

    Delete old script

--- scripts/bonsai_prompt_projection_deterministic.py
+++
@@ -1,9 +0,0 @@
-def deterministic_project_prompt_hidden(hidden):
-    from bonsai_text_consts import BONSAI_CONTEXT_DIM,TEXT_HIDDEN_DIM
-    if hidden.ndim!=3:
-        raise ValueError('hidden rank')
-    if hidden.shape[-1]!=TEXT_HIDDEN_DIM:
-        raise ValueError('hidden dim')
-    x=hidden.float()
-    y=x.repeat(1,1,BONSAI_CONTEXT_DIM//TEXT_HIDDEN_DIM)
-    return y
