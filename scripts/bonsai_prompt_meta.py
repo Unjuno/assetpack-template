8754939394547dd6e81895d119e4757b70db4f1e
Unjuno <unjuno.org@unjuno.org>
2026-06-14 22:29:36 +0900

    Delete old script

--- scripts/bonsai_prompt_meta.py
+++
@@ -1,5 +0,0 @@
-from bonsai_prompt_adapter_kind import adapter_name
-
-
-def prompt_meta():
-    return {'adapter':adapter_name(),'loop_mode':'per_step_repredict','claim':'ci_smoke_not_full_prompt_conditioning'}
