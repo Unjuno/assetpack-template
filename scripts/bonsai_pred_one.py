2370254f61aebc39ca7c6563b6a4958ab9d41483
Unjuno <unjuno.org@unjuno.org>
2026-06-14 22:29:16 +0900

    Delete old script

--- scripts/bonsai_pred_one.py
+++
@@ -1,8 +0,0 @@
-from bonsai_ctx_cast import ctx_like
-from bonsai_lowbit_step import load_sd,step
-from bonsai_prompt_seed import seeded_inputs
-
-
-def pred_once():
-    sd=load_sd(); z,t=seeded_inputs(); c=ctx_like(z)
-    return z,c,step(sd,z,c,t)
